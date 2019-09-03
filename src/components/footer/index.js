80e75ca8642e0f4e60051ac57b3dcb6962057a33
Fernán Ramos <framos@mrmilu.com>
2019-09-03 17:00:25 +0200

    Added footer ONU-8

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -6,35 +6,56 @@ import logo from "../../assets/images/logo-footer.svg";
 
 const Footer = ({}) => (
   <footer className="footer">
-    <img className="footer__logo" src={logo} alt="footer logo" />
-    <div className="footer__links">
-      <ul>
-        <li>
-          <a href="#">Contact</a>
-        </li>
-        <li>
-          <a href="#">Copyright</a>
-        </li>
-        <li>
-          <a href="#">Fraud Alert</a>
-        </li>
-        <li>
-          <a href="#">Privacy Notice</a>
-        </li>
-        <li>
-          <a href="#">Site Index</a>
-        </li>
-        <li>
-          <a href="#">Terms of Use</a>
-        </li>
-      </ul>
-      <div className="footer__links-social">
-        <a href="#">
-          <img src={facebook} alt="facebook logo" />
-        </a>
-        <a href="#">
-          <img src={twitter} alt="facebook logo" />
-        </a>
+    <div className="container">
+      <div className="row d-flex align-items-center">
+        <div className="col-sm-12  col-lg-4">
+          <img className="footer__logo" src={logo} alt="footer logo" />
+        </div>
+        <div className="col-sm-12  col-lg-8">
+          <div className="footer__links d-flex">
+            <ul class="nav">
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Contact
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Copyright
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Fraud Alert
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Privacy Notice
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Site Index
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Terms of Use
+                </a>
+              </li>
+            </ul>
+            
+            <div className="footer__links-social d-flex">
+              <a href="#">
+                <img src={facebook} alt="facebook logo" />
+              </a>
+              <a href="#">
+                <img src={twitter} alt="facebook logo" />
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </footer>
