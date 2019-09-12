4ec289323903ff662a5fdc34d33d151c546ae84a
Fernán Ramos <framos@mrmilu.com>
2019-09-12 15:46:54 +0200

    BUILD

--- src/pages/page-news.js
+++ src/pages/page-news.js
@@ -1,14 +1,20 @@
 import React from "react";
 import { Link } from "gatsby";
-import Layout from "../components/layout";
 const NewsPage = () => (
-  <Layout>
-    <Link to="news/page-news-01">News-01</Link>
-    <Link to="news/page-news-02/">News-02</Link>
-    <Link to="news/page-news-03/">News-03</Link>
-    <Link to="news/page-news-04/">News-04</Link>
-    <Link to="news/page-news-05/">News-05</Link>
-  </Layout>
+  <>
+    <div className="container">
+      <div className="heading-block">
+        <h2>List of subcomponents</h2>
+      </div>
+    </div>
+    <div className="container d-flex flex-column">
+      <Link to="news/page-news-01">News-01</Link>
+      <Link to="news/page-news-02/">News-02</Link>
+      <Link to="news/page-news-03/">News-03</Link>
+      <Link to="news/page-news-04/">News-04</Link>
+      <Link to="news/page-news-05/">News-05</Link>
+    </div>
+  </>
 );
 
 export default NewsPage;
