ea8b1cba5d9314072615412b4c63de461e579482
Fernán Ramos <framos@mrmilu.com>
2019-09-13 08:06:46 +0200

    Work in goals

--- src/components/Goals/Goal/index.js
+++ src/components/Goals/Goal/index.js
@@ -9,62 +9,62 @@ const Goal = ({ goal }) => (
     data-toggle="modal"
     data-target={`#${goal}`}
   >
-    <a href="#">
-      <img src={goalImage} className="goal-image" alt="goal image" />
-      <div
-        className="modal fade"
-        id={goal}
-        tabindex="-1"
-        role="dialog"
-        aria-labelledby="exampleModalLabel"
-        aria-hidden="true"
-      >
-        <div class="modal-dialog" role="document">
-          <button
-            type="button"
-            class="close"
-            data-dismiss="modal"
-            aria-label="Close"
-          >
-            <span aria-hidden="true">&times;</span>
-          </button>
+    <div
+      className="modal fade d-xs-none "
+      id={goal}
+      tabindex="-1"
+      role="dialog"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog" role="document">
+        <button
+          type="button"
+          class="close"
+          data-dismiss="modal"
+          aria-label="Close"
+        >
+          <span aria-hidden="true">&times;</span>
+        </button>
 
-          <div class="modal-body">
-            <div className="card-body">
-              <span className="goal-number">8</span>
-              <h3 className="goal-title">
-                SAMOA Pathway High-Level Midterm Review 2019
-              </h3>
-              <p className="goal-text">
-                End hunger, achieve food security and improved nutrition and
-                promote sustainable agriculture.
-              </p>
-              <div className="goal-info">
-                <div className="col-6">
-                  <span className="number">15</span>
-                  <p className="feature">targets</p>
-                </div>
-                <div className="col-6">
-                  <span className="number">608</span>
-                  <p className="feature">targets</p>
-                </div>
-                <div className="col-6">
-                  <span className="number">20</span>
-                  <p className="feature">targets</p>
-                </div>
-                <div className="col-6">
-                  <span className="number">35</span>
-                  <p className="feature">targets</p>
-                </div>
+        <div class="modal-body">
+          <div className="card-body">
+            <span className="goal-number">8</span>
+            <h3 className="goal-title">
+              SAMOA Pathway High-Level Midterm Review 2019
+            </h3>
+            <p className="goal-text">
+              End hunger, achieve food security and improved nutrition and
+              promote sustainable agriculture.
+            </p>
+            <div className="goal-info">
+              <div className="col-6">
+                <span className="number">15</span>
+                <p className="feature">targets</p>
               </div>
-              <button type="button" className="btn btn-goal">
-                More info
-              </button>
+              <div className="col-6">
+                <span className="number">608</span>
+                <p className="feature">targets</p>
+              </div>
+              <div className="col-6">
+                <span className="number">20</span>
+                <p className="feature">targets</p>
+              </div>
+              <div className="col-6">
+                <span className="number">35</span>
+                <p className="feature">targets</p>
+              </div>
             </div>
+            <button type="button" className="btn btn-goal">
+              More info
+            </button>
           </div>
         </div>
       </div>
+    </div>
 
+    <a href="#">
+      <img src={goalImage} className="goal-image" alt="goal image" />
       <div className="card-body">
         <span className="goal-number">8</span>
         <h3 className="goal-title">
