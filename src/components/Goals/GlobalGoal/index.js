6c8b84262d9caf15a712c95bf4e7570457a009d0
Fernan-Ramos <ferburgos93@gmail.com>
2019-09-20 08:07:54 +0200

    Fixed button apparence

--- src/components/Goals/GlobalGoal/index.js
+++ src/components/Goals/GlobalGoal/index.js
@@ -5,12 +5,14 @@ import "./style.scss";
 const GlobalGoal = ({}) => (
   <div className="card  goal-global col-6 col-sm-6 col-md-4 col-lg-3 col-xl-2">
     <a href="#">
-      <img
-        src="/assets/goals/global-goals.png"
-        className="goal-image"
-        alt="goal image"
-      />
-      <Link>See all</Link>
+     
+        <img
+          src="/assets/goals/global-goals.png"
+          className="goal-image"
+          alt="goal image"
+        />
+        <Link>See all</Link>
+      
     </a>
   </div>
 );
