45de20872d94e44fbf22fe8cb23792b1134aca65
Fernan-Ramos <ferburgos93@gmail.com>
2019-09-20 12:15:03 +0200

    Changed navbar by navbar old

--- src/components/Header/HeaderBannerV2/index.js
+++ src/components/Header/HeaderBannerV2/index.js
@@ -6,12 +6,32 @@ const HeaderBannerV2 = ({}) => (
   <div className="container">
     <div className="row header-banner header-form">
       <div className="col-lg-8 col-md-12 d-flex  mb-3 mb-lg-0">
-        <img className="d-none d-lg-block logo" src='/assets/images/logo.svg' alt="logo" />
-        <img className="d-lg-none logo-mobile" src="/assets/images/logo_mobile.svg" alt="logo" />
+        <img
+          className="d-none d-lg-block logo"
+          src="/assets/images/logo.svg"
+          alt="logo"
+        />
+        <img
+          className="d-lg-none logo-mobile"
+          src="/assets/images/logo_mobile.svg"
+          alt="logo"
+        />
       </div>
       <div className="col-lg-4 col-md-12">
-        <div className="row">
+        <div className="row align-items-center">
           <div className="col-8">
+            <form className="d-flex mb-1">
+              <input
+                className="form-control"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button className="btn" type="submit">
+                <img src="/assets/images/search.svg" />
+              </button>
+            </form>
+            {/*
             <form className="d-flex mb-1">
               <input
                 className="form-control"
@@ -23,15 +43,24 @@ const HeaderBannerV2 = ({}) => (
                 Search
               </button>
             </form>
+            */}
           </div>
           <div className="col">
             <div className="actions d-xs-flex  justify-content-md-end">
               <button className="button button-circle grey">
-                <img className="svg" src='/assets/images/bell.svg' alt="bell image" />
+                <img
+                  className="svg"
+                  src="/assets/images/bell.svg"
+                  alt="bell image"
+                />
                 <Notification>10</Notification>
               </button>
               <button className="button button-circle grey">
-                <img className="svg" src='/assets/images/user.svg' alt="user image" />
+                <img
+                  className="svg"
+                  src="/assets/images/user.svg"
+                  alt="user image"
+                />
               </button>
             </div>
           </div>
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -12,7 +12,7 @@ class Header extends React.Component {
       <header>
         <LanguageNavBar />
         <HeaderBannerV2 />
-        <NavBarFull />
+        <NavBar />
       </header>
     );
   }
