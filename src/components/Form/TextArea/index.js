3a0a4f13f511a09057068014ef03dcef7bd08d4f
Fernán Ramos <framos@mrmilu.com>
2019-09-27 11:16:00 +0200

    Fix inputs

--- src/components/Form/TextArea/index.js
+++ src/components/Form/TextArea/index.js
@@ -1,11 +1,19 @@
 import React from "react";
+import classNames from "classnames";
 import "./style.scss";
 
-const TextArea = ({ id, label, placeholder, subLabel }) => (
-  <div className="form-group">
-    <label  className="label" for={id}>{label}</label>
+const TextArea = ({ id, label, placeholder, subLabel, required, invalid , className}) => (
+  <div className={classNames("form-group", className)}>
+    <label className="label" for={id}>
+      {label} {required && <>*</>}
+    </label>
     {subLabel && <label className="sub-label">{subLabel}</label>}
-    <textarea name="textarea" className="form-control" placeholder={placeholder}></textarea>
+    <textarea
+      name="textarea"
+      className={classNames("form-control", { "is-invalid": invalid })}
+      placeholder={placeholder}
+    ></textarea>
+    <div className="invalid-feedback">This field is required</div>
   </div>
 );
 
--- src/pages/form/page-input.js
+++ src/pages/form/page-input.js
@@ -2,10 +2,58 @@ import React from "react";
 
 import Container from "../../components/container";
 import Input from "../../components/Form/Input";
+import TextArea from "../../components/Form/TextArea";
 
 const InputPage = () => (
   <Container>
-   <Input/>
+    <form className="form-un row">
+      <Input
+        id="normall"
+        type="text"
+        placeholder="Text"
+        label="Input"
+        className="col-6"
+      />
+      <Input
+        id="invalid"
+        type="text"
+        placeholder="Text"
+        label="Input"
+        className="col-6"
+        required="true"
+        invalid
+      />
+      <Input
+        type="seach"
+        placeholder="Text"
+        label="Input"
+        subLabel="Please clearly describe how this initiative is carried out"
+        className="col-6"
+      />
+      <Input
+        type="text"
+        placeholder="Text"
+        label="Input"
+        subLabel="Please clearly describe how this initiative is governed"
+        className="col-6"
+      />
+      <Input
+        type="date"
+        placeholder="dd/mm/yyyy"
+        label="Start date"
+        subLabel="Enter the star date (Year Month) of the iniciative"
+        className="col-6"
+        required
+      />
+      <TextArea 
+       placeholder="Description (max. 100 words)"
+       label="TextArea"
+       subLabel="Please describe as clearly as possible what the objective of this initiative/partnership/commitment is."
+       className="col-7"
+       required="true"
+       invalid
+      />
+    </form>
   </Container>
 );
 
